9a366939f2da407329f6922b00dfb1d624d649d1
KatherineJY <kellymayliu@163.com>
2018-10-20 21:04:41 +0800

    异步加载数据

--- echarts-learning/js/test1.js
+++ echarts-learning/js/test1.js
@@ -1,23 +1,50 @@
-(function(){
-    let myChart = echarts.init(document.getElementById("chart"));
-    let options = {
-        title:{
-            text:'score'
-        },
-        legend:{
-            data:['xiaoliang']
-        },
-        xAxis:{
-            data:[60,70,80,90,100]
-        },
-        yAxis:{
+function fetchData(cb) {
+    // 通过 setTimeout 模拟异步加载
+    setTimeout(function () {
+        cb({
+            categories: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
+            data: [5, 20, 36, 10, 10, 20]
+        });
+    }, 3000);
+}
 
+// 初始 option
+option = {
+    title: {
+        text: '异步数据加载示例'
+    },
+    tooltip: {},
+    legend: {
+        data:['销量']
+    },
+    xAxis: {
+        data: []
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: []
+    }]
+};
+let myChart = echarts.init(document.getElementById('chart'));
+myChart.showLoading();
+
+fetchData(function (data) {
+    myChart.hideLoading();
+    myChart.setOption(option);
+    myChart.setOption({
+        xAxis: {
+            data: data.categories
         },
-        series:[{
-            name:'xiaoliang',
-            type:'bar',
-            data:['12','34','45','21','1']
+        series: [{
+            // 根据名字对应到相应的系列
+            name: '销量',
+            data: data.data
         }]
-    };
-    myChart.setOption(options);
-})();
+    });
+});
+
+/*if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}*/
