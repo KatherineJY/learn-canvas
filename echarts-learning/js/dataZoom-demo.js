556e230b57721d69819bad016ca343870af9b010
KatherineJY <kellymayliu@163.com>
2018-10-21 20:05:15 +0800

    dataZoom

--- echarts-learning/js/dataZoom-demo.js
+++ echarts-learning/js/dataZoom-demo.js
@@ -9,6 +9,25 @@
         dataZoom: [
             {
                 type: 'slider',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'inside',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'slider',
+                yAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'inside',
+                yAxisIndex: 0,
                 start: 10,
                 end: 60
             }
